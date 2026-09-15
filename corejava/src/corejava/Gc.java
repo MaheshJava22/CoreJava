74d1f828fde265379ed55a6c3c6442a6841a0b2b
mahes <mahes@DESKTOP-38GD0V6>
2026-09-15 10:41:25 +0530

    garbage collector program

---
+++ corejava/src/corejava/Gc.java
@@ -0,0 +1,13 @@
+package corejava;
+import java.util.Scanner;
+public class Gc {
+	public static void main(Strings args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the value of N :");
+		System.out.println("enter the value of N :");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println(a+b);
+		
+	}
+}
