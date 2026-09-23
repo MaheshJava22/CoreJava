9413bce562edf586b93c987228442c6bd2cfd0b7
mahes <mahes@DESKTOP-38GD0V6>
2026-09-23 09:55:37 +0530

    Day 2 of Assignment

---
+++ corejava/src/corejava/Assignment3.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		int Chocolate=15; int Cookie=10;
+		int Total=450;
+		int Chocos= Chocolate*10; int Cookies= Cookie*5;
+		System.out.println("money Spent on Chocolates = " +  Chocos);
+		System.out.println("money Spent on Cookies = " +  Cookies);
+		 int Remain= Total-Chocos-Cookies;
+		 System.out.println("Remaining money after the purchase = " +  Remain);
+
+	}
+
+}
